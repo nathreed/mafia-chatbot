94eacdb523ab584e0ee71c833c7cb73ee0ffa00f
Nathan Reed <nathreed@gmail.com>
2018-10-27 00:35:53 -0400

    switch to post method because that is what slack is sending

--- server.js
+++ server.js
@@ -5,22 +5,21 @@ const app = express();
 const port = 8080;
 
 //Function gets called when we get a start game command from slack
-app.get("/cmd/startgame", function (req,res) {
+app.post("/cmd/startgame", function (req,res) {
 	console.log("startgame command");
-	res.statusCode(200);
-	res.send("hello world")
+	res.send("startgame")
 });
 
 //Function gets called when we get a getrules comamnd from slack
-app.get("/cmd/getrules", function (req,res) {
+app.post("/cmd/getrules", function (req,res) {
 	console.log("getrules command");
-	res.statusCode(200);
+	res.send("setrules");
 });
 
 //Function gets called when we get an accuse command from slack
-app.get("/cmd/accuse", function (req, res) {
+app.post("/cmd/accuse", function (req, res) {
 	console.log("accuse command");
-	res.statusCode(200);
+	res.send("accuse");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));

6cc1f4f621a00f29cd2844c70f316793bd9e4c1d
Nathan Reed <nathreed@gmail.com>
2018-10-27 10:21:10 -0400

    began to set up code for the startgame command

--- server.js
+++ server.js
@@ -1,15 +1,21 @@
 const express = require("express");
+const https = require("https");
+const qs = require("querystring");
 
 const app = express();
 
 const port = 8080;
+
+const botToken = require("./secrets")["bot-token"];
 
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.post("/events", function(req, res) {
     console.log(req.body);
-    res.send(req.body.challenge);
+    //Slack events url verification code
+    //res.send(req.body.challenge);
+
 });
 
 //Function gets called when we get a start game command from slack
@@ -17,6 +23,7 @@ app.post("/cmd/startgame", function (req,res) {
 	console.log("startgame command");
 	res.send("startgame");
     console.log(req.body);
+
 
 
     /*
@@ -28,6 +35,43 @@ app.post("/cmd/startgame", function (req,res) {
 
     - need to think about timing and how that works (? - do we even want a timing feature?)
      */
+
+    let channelID = req.body["channel_id"];
+
+    //Step 1: Grab list of every user in the chat so we can check if they are active
+
+    const encodedData = qs.stringify({
+        token: botToken,
+        channel: channelID,
+    });
+
+    const options = {
+        hostname: "slack.com",
+        path: "/api/conversations.members?"+encodedData
+    };
+
+    console.log("about to send request to", options.path);
+
+
+
+    https.get(options, function(res) {
+        let fullData = "";
+        res.on("data", function(data) {
+            fullData += data;
+        });
+
+        res.on("error", function(err) {
+            console.log("Error with request to list people in the channel:", err);
+        });
+
+        res.on("end", function() {
+            //We have all the data, now do stuff with it
+            console.log(botToken);
+            console.log(fullData);
+        });
+    });
+
+
 
 });
 

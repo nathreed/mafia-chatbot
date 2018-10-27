77d2bdad5b5be10bf69a365f6f6c27801719b573
Nathan Reed <nathreed@gmail.com>
2018-10-27 00:20:35 -0400

    send http 200, maybe that makes it work with slack?

--- server.js
+++ server.js
@@ -7,16 +7,19 @@ const port = 8080;
 //Function gets called when we get a start game command from slack
 app.get("/cmd/startgame", (req, res) => {
 	console.log("startgame command");
+	res.statusCode(200);
 });
 
 //Function gets called when we get a getrules comamnd from slack
 app.get("/cmd/getrules", (req, res) => {
 	console.log("getrules command");
+	res.statusCode(200);
 });
 
 //Function gets called when we get an accuse command from slack
 app.get("/cmd/accuse", (req,res) => {
-	console.log("accuse command")
+	console.log("accuse command");
+	res.statusCode(200);
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

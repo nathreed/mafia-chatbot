da40102c138216a030bad441067c83f91a760c8b
Nathan Reed <nathreed@gmail.com>
2018-10-27 00:28:32 -0400

    change function structure to enable us to send http200 properly

--- server.js
+++ server.js
@@ -5,19 +5,20 @@ const app = express();
 const port = 8080;
 
 //Function gets called when we get a start game command from slack
-app.get("/cmd/startgame", (req, res) => {
+app.get("/cmd/startgame", function (req,res) {
 	console.log("startgame command");
 	res.statusCode(200);
+	res.send("hello world")
 });
 
 //Function gets called when we get a getrules comamnd from slack
-app.get("/cmd/getrules", (req, res) => {
+app.get("/cmd/getrules", function (req,res) {
 	console.log("getrules command");
 	res.statusCode(200);
 });
 
 //Function gets called when we get an accuse command from slack
-app.get("/cmd/accuse", (req,res) => {
+app.get("/cmd/accuse", function (req, res) {
 	console.log("accuse command");
 	res.statusCode(200);
 });
